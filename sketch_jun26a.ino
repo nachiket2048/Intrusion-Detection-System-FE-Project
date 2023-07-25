17f11538803f853c8c36fc9605931e04846111de
Nachiket Markale <130375886+nachiket2048@users.noreply.github.com>
2023-07-25 18:31:42 +0530

    Add files via upload

---
+++ sketch_jun26a.ino
@@ -0,0 +1,19 @@
+const int pirPin = 2;     // PIR sensor output pin
+const int buzzerPin = 3;  // Buzzer control pin
+
+void setup() {
+  pinMode(pirPin, INPUT);      // Set PIR pin as input
+  pinMode(buzzerPin, OUTPUT);  // Set buzzer pin as output
+  Serial.begin(9600);          // Initialize serial communication
+}
+
+void loop() {
+  int pirValue = digitalRead(pirPin);  // Read PIR sensor value
+
+  if (pirValue == HIGH) {  // If motion is detected
+    Serial.println("Motion detected!");
+    digitalWrite(buzzerPin, HIGH);  // Turn on the buzzer
+    delay(3000);                     // Wait for 3 seconds
+    digitalWrite(buzzerPin, LOW);   // Turn off the buzzer
+  }
+}
